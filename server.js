967e7327f2e630bf8b8f7cf1cc48c66e2a2a31e2
Rob <rschumann88@gmail.com>
2019-11-19 00:48:40 -0500

    post route

--- server.js
+++ server.js
@@ -6,16 +6,22 @@ const PORT = process.env.PORT || 8000;
 const MongoClient = require('mongodb').MongoClient;
 const mongoURL = "mongodb://localhost:27017/HomeworkFinalProject";
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('combined', { stream: accessLogStream }));
+app.use(bodyParser.json());
+
+// query results
+let collection;
 
 const connectDB = async () => {
   try {
     const connection = await MongoClient.connect(mongoURL);
     const db = connection.db('customersDB');
-    const collection = await db.createCollection('customers');
+    collection = await db.createCollection('customers');
+    console.log('Connected');
   } catch (err) {
     console.log(err);
     process.exit(1);
@@ -23,4 +29,36 @@ const connectDB = async () => {
 };
 
 connectDB();
+
+// return all customers
+app.get('/api/customers', async (req, res) => {
+  try {
+    const result = await collection.find().toArray();
+    res.end(JSON.stringify(result));
+  } catch (err) {
+    res.status(500);
+    console.log(err);
+    res.end(JSON.stringify({ status: "error" }));
+  }
+});
+
+app.post('/api/customers', async (req, res) => {
+  if (req.headers['content-type'] != 'application/json') {
+    res.status(416);
+    res.end(JSON.stringify({status: 'incorrect content-type'}));
+  }
+  try {
+    const result = await collection.insertOne(req.body);
+    res.status(201);
+    const response = {
+      resultID: `${result.insertedId}`
+    };
+    res.end(JSON.stringify(response));
+  } catch (err) {
+    res.status(500);
+    console.log(err);
+    res.end(JSON.stringify({ status: 'error' }));
+  }
+});
+
 app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
